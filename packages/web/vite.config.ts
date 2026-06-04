a2cbc11c3311c8e156ce19986112c04dd24fd468
omisoreebenezer-glitch <omisoreebenezer@gmail.com>
2026-06-05 00:09:33 +0100

    Update vite.config.ts

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,28 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwind from "@tailwindcss/vite"
-import path from "path";
-import honoDevPlugin from "./vite/plugins/hono-dev-plugin";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-const root = path.resolve(__dirname, "../..");
-
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, root, '');
-	Object.assign(process.env, env);
-		resolve: {
-			alias: {
-				"@": path.resolve(__dirname, "./src/web"),
-			},
-		},
-		server: {
-			allowedHosts: true,
-			port: 4200,
-			host: "0.0.0.0",
-			hmr: { overlay: false, },
-			cors: true,
-			headers: {
-				"X-Frame-Options": "ALLOWALL",
-			}
-		}
-	};
-});
+export default defineConfig({
+  plugins: [react()]
+})
